5304208f7ac333e1ed298bec7e90c906635ee4b7
AzeezAAlimi <azeezaalimi@gmail.com>
2025-02-04 21:52:55 +0100

    fix

--- src/utilities/getAccessToken.ts
+++ src/utilities/getAccessToken.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 async function getAccessToken(request: any): Promise<string> {
-  const CLIENT_ID = '85833f6f28d84050894f2cfbd133d660';
-  const CLIENT_SECRET = 'a62d5e15de6049339cf685893f001333';
+  const CLIENT_ID = 'xxxxx';
+  const CLIENT_SECRET = 'xxxx';
 
   const response = await request.post('https://accounts.spotify.com/api/token', {
     headers: {
