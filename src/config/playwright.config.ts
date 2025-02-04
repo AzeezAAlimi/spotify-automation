0ee7990714be46413f959398fc9590650d452a3f
AzeezAAlimi <azeezaalimi@gmail.com>
2025-02-04 11:38:25 +0100

    update config

---
+++ src/config/playwright.config.ts
@@ -0,0 +1,28 @@
+import type { PlaywrightTestConfig } from '@playwright/test';
+import { devices } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  testDir: './tests/spotify',
+  retries: 3,
+  workers: 2,
+  timeout: 30000, // for the overral test
+  reporter: [['html', { outputFolder: 'playwright-report' }]],
+  use: {
+    headless: true,
+    ignoreHTTPSErrors: true,
+    actionTimeout: 15000,
+    trace: 'retain-on-failure',
+  },
+  expect: {
+    timeout: 900000, // for assertions
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { browserName: 'chromium' },
+    },
+  ],
+};
+
+export default config;
